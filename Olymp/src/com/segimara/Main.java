7546ab7e59ad4b89c201f0379835a4cd04495bbe
Segimara <76587473+Segimara@users.noreply.github.com>
2022-10-20 23:19:27 +0300

    some shit...

--- Olymp/src/com/segimara/Main.java
+++ Olymp/src/com/segimara/Main.java
@@ -1,8 +1,14 @@
 package com.segimara;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int[] a = new int[3];
+        for (int i = 0; i < 3; i++) {
+            a[i] = scanner.nextInt();
+        }
     }
 }

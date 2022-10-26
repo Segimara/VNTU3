24297752b114d0700f1a74aca332073329cee78e
Segimara <76587473+Segimara@users.noreply.github.com>
2022-10-27 00:16:19 +0300

    Update StatichashTable.cs

--- VNTU/ASD/Lab5/StatichashTable.cs
+++ VNTU/ASD/Lab5/StatichashTable.cs
@@ -8,35 +8,44 @@ namespace ASD.Lab5
 {
     internal class StatichashTable<Key, T>
     {
-        private List<LinkedList<Key>> Keys = new List<LinkedList<Key>>(13);
-        private List<LinkedList<T>> Values = new List<LinkedList<T>>(13);
+        private List<List<Key>> Keys = new List<List<Key>>(13);
+        private List<List<T>> Values = new List<List<T>>(13);
 
         public bool Add((Key, T) elem)
         {
-            int index = elem.Item1.GetHashCode() % 13;
+            int index = getHash(elem.Item1);
             if (Values[index] == null)
             {
-                Values[index] = new LinkedList<T>();
+                Values[index] = new List<T>();
             }
             if (Keys[index] == null)
             {
-                Keys[index] = new LinkedList<Key>();
+                Keys[index] = new List<Key>();
             }
             if (!(Keys[index].Select(m => m.GetHashCode()).Contains(elem.Item1.GetHashCode())))
             {
-                Keys[index].AddLast(elem.Item1);
-                Values[index].AddLast(elem.Item2);
+                Keys[index].Add(elem.Item1);
+                Values[index].Add(elem.Item2);
                 return true;    
             }
             return false;
         }
-        //public bool Remove()
+        public bool Remove(Key key)
+        {
+            int index = getHash(key);
+            int secIndex = Keys[index].FindIndex(0, m => m.GetHashCode() == key.GetHashCode());
+            if (secIndex == -1)
+                return false;
+            Values[index].RemoveAt(secIndex);
+            Keys[index].RemoveAt(secIndex);
+            return true;
+        }
         public T this[Key key]
         {
             get
             {
                 int secIndex = -1;
-                int index = key.GetHashCode() % 13;
+                int index = getHash(key);
                 if (Keys[index] == null)
                     throw new IndexOutOfRangeException();
                 for (int i = 0; i < Keys[index].Count; i++)
@@ -51,12 +60,32 @@ namespace ASD.Lab5
                 return Values[index].ElementAt(secIndex);
             }
         }
-
+        private static int getHash(Key key)
+        {
+            return key.GetHashCode() % 13;
+        }
         public void FillRandom(int count = 8)
         {
             Random r = new Random();
 
         }
 
+    }
+
+
+
+    class Lab5
+    {
+        public static void Main5()
+        {
+            while(true)
+            {
+                PrintMenu();
+            }
+        }
+        public static void PrintMenu()
+        {
+            Console.WriteLine();
+        }
     }
 }
